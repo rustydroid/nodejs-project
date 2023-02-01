b7dbafa86801834b4745b062fb14c6b8a60ecdfe
Marcelo Cardozo <marcelorcardozo@gmail.com>
2023-02-01 00:26:49 -0300

    Adding updateProduct

--- productManager.js
+++ productManager.js
@@ -10,6 +10,8 @@ class ProductManager {
     this.encoding = "utf-8";
     this.pathDir = "./db";
     this.path = this.pathDir + "/products.json";
+    // this.path = "./products.json";
+    this.fileReaded = false;
   }
 
   // Check if Product code already exist in array
@@ -20,8 +22,73 @@ class ProductManager {
     return checkExist;
   };
 
+  // Check if directory exist
+  // checkDirExist = async () => {
+  //   await fs.access(this.pathDir, (err) => {
+  //     if (err) {
+  //       console.log("El directorio no existe: ");
+  //       fs.mkdir(this.pathDir, { recursive: true }, (err) => {
+  //         if (err) throw Error("No se pudo crear el directorio");
+  //         else console.log("Directorio creado!!");
+  //       });
+  //     }
+  //   });
+  // };
+
+  // Check if products.json exist
+  checkFileExist = async () => {
+    await fs.promises.access(this.path, (err) => {
+      if (err) {
+        console.error("File don't exist");
+        throw Error(`File don't exist on path provided ${this.path}`);
+      }
+    });
+  };
+
+  // Read products from file
+  readProducts = async () => {
+    if (!this.fileReaded) {
+      try {
+        await this.checkFileExist;
+        let productsRead = await fs.promises.readFile(this.path, this.encoding);
+        console.log("Products readed from JSON file");
+        this.products = JSON.parse(productsRead);
+        this.fileReaded = true;
+      } catch (error) {
+        console.error(
+          `Error reading products from file: ${this.path}, ${error}`
+        );
+        throw Error(`Error reading products from file: ${this.path}, ${error}`);
+      }
+    }
+  };
+
+  // Save products to file
+  saveProducts = async () => {
+    try {
+      // this.checkDirExist();
+      await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(this.products),
+        (err) => {
+          if (err) {
+            console.error(
+              `Error saving products on file: ${this.path}, ${error}`
+            );
+            throw Error(
+              `Error saving products on file: ${this.path}, ${error}`
+            );
+          }
+        }
+      );
+      console.log("Products saved on file!!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // Add a product checking if the code exist or not
-  addProduct = (title, desc, price, thumb, code, stock) => {
+  addProduct = async (title, desc, price, thumb, code, stock) => {
     if (!this.existProduct(code)) {
       let newProduct = new Product(
         this.#idControl,
@@ -34,35 +101,29 @@ class ProductManager {
       );
       this.products.push(newProduct);
       this.#idControl++;
-      fs.access(this.pathDir, (err) => {
-        if (err) {
-          fs.mkdir(this.pathDir, { recursive: true }, (err) => {
-            if (err) throw Error("No se pudo crear el directorio");
-            else console.log("Directorio creado!!");
-          });
-        }
-        fs.writeFile(this.path, JSON.stringify(this.products), (err) => {
-          if (err) throw Error("No se pudo salvar el archivo");
-          else console.log("Se guardaron los productos correctamente!!");
-        });
-      });
-    } else {
-      console.log("Codigo duplicado");
     }
+    await this.saveProducts();
+  };
+
+  // Update product by Id
+  updateProduct = async (id, productUpdate) => {
+    await this.readProducts();
+    console.log("Funcion update Product");
+    const parseUpdate = this.products.map((product) => {
+      if (product.id === id) return { ...product, ...productUpdate };
+      else return product;
+    });
+    this.products = parseUpdate;
+    console.log("Objetos actualizados");
+    await this.saveProducts();
   };
 
   // Return all products in array
-  getProducts = () => {
-    fs.access(this.path, (err) => {
-      if (err) throw Error("File productos no existe!");
-      fs.readFile(this.path, this.encoding, (err, content) => {
-        if (err) throw Error("No se pudo leer el archivo");
-        this.products = [];
-        this.products = JSON.parse(content);
-        console.log("Listado Productos");
-        console.log(this.products);
-      });
-    });
+  getProducts = async () => {
+    await this.readProducts();
+    console.log("List of All products");
+    console.log("========================");
+    console.log(this.products);
   };
 
   // Search if Product id exist in array, if not return a message
@@ -87,14 +148,13 @@ class ProductManager {
     });
   };
 
-  // 
+  //
 }
 
 // TESTING
+
 // New ProductManager instance
 let productManager = new ProductManager();
-// Calling getProducts
-productManager.getProducts();
 
 // Calling addProduct with dummy data
 productManager.addProduct(
@@ -133,3 +193,7 @@ productManager.addProduct(
 productManager.getProductById(0);
 // Search by Product id - Return error "Producto Inexistente"
 productManager.getProductById(5);
+
+// Modify a product based on id
+productManager.updateProduct(1, { title: "Nuevo Libro", price: 5000 });
+
